de6f29da655985e45b4211b35930e918ffaa84b6
sdavitt <sdavt2@gmail.com>
2022-04-21 13:44:30 -0500

    initial

---
+++ gitdemo.py
@@ -0,0 +1,9 @@
+# Some football variables
+favorite_team = 'Manchester City'
+favorite_player = 'Joao Cancelo'
+
+transfer_target = 'Erling Haaland'
+
+lineup = {'CB': 'Ruben Dias', 'K': 'Ederson Moraes'}
+
+print(lineup)
